be3bbeed13b382714bfd03735686121e37b73535
Mitch Z <destructoglitch@gmail.com>
2018-04-06 13:34:55 -0400

    Fixed bug that caused atrobots to not be launched

--- robotselection.cpp
+++ robotselection.cpp
@@ -1,15 +1,20 @@
 #include "robotselection.h"
 #include "ui_robotselection.h"
 #include <QFileDialog>
+#include <QFileInfo>
 #include <QList>
 #include <QMessageBox>
 #include <QProcess>
 #include <QDebug>
 
-#include <cstdlib>
+#ifdef _WIN32
+    #include <Windows.h>
+#endif
+
+#include <stdlib.h>
 
 QString fName;
-QString command = "./atr2.exe";
+QString command = "atr2.exe";
 QString cmditems;
 
 RobotSelection::RobotSelection(QWidget *parent) :
@@ -37,7 +42,27 @@ void RobotSelection::on_pushButton_2_clicked()
 
 void RobotSelection::on_pushButton_3_clicked()
 {
-    command = "./atr2.exe";
+#ifdef _WIN32
+    TCHAR   buffer[MAX_PATH];
+    wchar_t file[11];
+    char    path[MAX_PATH];
+    LPCWSTR pointer;
+
+    mbstowcs(file, "./atr2.exe", strlen("./atr2.exe")+1);
+
+    pointer = file;
+
+    GetFullPathName(pointer, MAX_PATH, buffer, NULL);
+
+    wcstombs(path, buffer, MAX_PATH);
+
+    command = path;
+
+#else
+    command = realpath("./atr2.exe", NULL);
+#endif
+
+
 
     for(int i = 0; i < ui->listWidget->count(); i++){
         cmditems += ui->listWidget->item(i)->text() + " ";
@@ -45,7 +70,6 @@ void RobotSelection::on_pushButton_3_clicked()
     command = command + " " + cmditems;
     cmditems = "";
 
-    QMessageBox::about(this, "DEBUG", command);
     /*QStringList arguments;
     arguments << command;
     QProcess exec;
@@ -53,7 +77,12 @@ void RobotSelection::on_pushButton_3_clicked()
     exec.waitForFinished();
     */
     std::string temp = command.toStdString();
-   system(temp.c_str());
+
+    QString qstr = QString::fromStdString(temp);
+    //QMessageBox::about(this, "DEBUG", qstr);
+    system((command.toStdString()).c_str());
+
+
 
 
    ui->listWidget->clear();
